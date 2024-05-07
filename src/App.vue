a01c0b660a68564cdcdfeeb58b4be907c40a85c9
WalterCorsini <corsini.walter@gmail.com>
2024-05-07 15:52:36 +0200

    fix function

--- src/App.vue
+++ src/App.vue
@@ -22,25 +22,42 @@ export default {
       .get("https://rickandmortyapi.com/api/character")
       .then((resp) => {
         this.store.list = resp.data.results;
+        this.cardsArray = this.store.list;
       })
     this.selectList();
   },
 
   methods: {
     selectList() {
+      //  methods with search in global storage
       this.cardsArray = [];
       if (this.store.selectedStatus !== "All") {
         this.store.list.forEach(element => {
           if (this.store.selectedStatus === element.status) {
-            // alert("si");
             this.cardsArray.push(element);
           }
         });
       } else {
+        console.log("ciao");
+        console.log(this.store.list);
         this.cardsArray = this.store.list;
       }
     }
   }
+
+  //  methods with call api when select options change
+
+  // let status = {};
+  // if(this.store.selectedStatus !== "All"){
+  //        status.status = this.store.selectedStatus;
+  //  }
+  //   axios
+  //   .get("https://rickandmortyapi.com/api/character",
+  //   {params: status})
+  //   .then((resp) => {
+  //     this.cardsArray = resp.data.results;
+  //   })
+  //
 };
 </script>
 
